1c0bc562bed130c9eeb5f2df7221c72d4eafa296
Sam Clark <samclark2399@gmail.com>
2026-05-13 17:48:51 -0400

    Made time to bet an env variable but defaults to 300 seconds

--- cogs/bet.py
+++ cogs/bet.py
@@ -5,6 +5,8 @@ import re
 import aiosqlite
 import random
 import string
+import os
+from dotenv import load_dotenv
 from itertools import combinations
 from helpers.model_helpers import predict_variable, predict_mp_total_rounds, predict_mp_player_rounds_all
 from helpers.bet_helpers import (
@@ -136,7 +138,10 @@ class Bet(commands.Cog):
             
             # COUNTDOWN SETUP
             
-            seconds = 180
+            load_dotenv()
+            bet_time = os.environ['BET_TIME_SECONDS']
+            seconds = bet_time if bet_time else 300
+
             minutes, secs = divmod(seconds, 60)
             bot_message = await ctx.send(
                 f"Bets for **{match_title}**\n"
